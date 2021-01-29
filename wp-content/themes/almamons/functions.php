9531f8eebfa5b5dd484b80a72a8fee5e9afb1d92
gocapuskarevic <gordana@axiomq.com>
2021-01-29 18:46:28 +0100

    Added variables for colors, line-height moved for nav.

--- wp-content/themes/almamons/functions.php
+++ wp-content/themes/almamons/functions.php
@@ -3,6 +3,8 @@
 function load_css() {
 	wp_register_style('main', get_template_directory_uri() . '/css/style.css', array(), false, 'all');
 	wp_enqueue_style('main');
+	wp_register_style('primary', get_template_directory_uri() . '/inc/css/style.css', array(), false, 'all');
+	wp_enqueue_style('primary');
 }
 add_action('wp_enqueue_scripts', 'load_css');
 
