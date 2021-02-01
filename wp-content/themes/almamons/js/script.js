a033ee7fa69d1d3f8500ab77feee05cbaee43701
gocapuskarevic <gordana@axiomq.com>
2021-02-01 21:33:12 +0100

    Style first li.

--- wp-content/themes/almamons/js/script.js
+++ wp-content/themes/almamons/js/script.js
@@ -1,51 +1,13 @@
-
-/*
-var CookieDate = new Date;
-CookieDate.setFullYear(CookieDate.getFullYear() +10);
-document.cookie = 'intro=done; expires=' + CookieDate.toUTCString() + ';';
-
-function getCookie(c_name) {
-	var c_value = document.cookie,
-			c_start = c_value.indexOf(" " + c_name + "=");
-	if (c_start == -1) c_start = c_value.indexOf(c_name + "=");
-	if (c_start == -1) {
-			c_value = null;
-	} else {
-			c_start = c_value.indexOf("=", c_start) + 1;
-			var c_end = c_value.indexOf(";", c_start);
-			if (c_end == -1) {
-					c_end = c_value.length;
-			}
-			c_value = unescape(c_value.substring(c_start, c_end));
-	}
-	return c_value;
-}
-*/
 jQuery(function($) {
 	jQuery( document ).ready(function(){
-		// my code goes here
-		
-			/*jQuery(".icon").on('click', function(e){
-				e.preventDefault();
-				jQuery(".navbar").toggleClass("show");
-			});
-	
-			jQuery("#open").on('click', function(e){
-				e.preventDefault();
-				jQuery(".sub-menu").toggleClass("show");
-			});*/
-			$('.sub-menu').css('display','none');
-			$('li.menu-item-has-children').on('click',function(){
-				if( $('.sub-menu').css('display') == 'block'){
-					$('.sub-menu').css('display','none');
-				}else if($('.sub-menu').css('display') == 'none'){
-					$('.sub-menu').css('display','block');
-				}
-			})
-	
-			/*var acookie = getCookie("intro");
-			if (!acookie) {
-					alert("Cookie not found.");
-			}else alert(acookie);*/
+		$('.sub-menu').css('display','none');
+		$('li.menu-item-has-children').on('click',function(){
+			console.log($('.sub-menu').css('display'))
+			if( $('.sub-menu').css('display') == 'block'){
+				$('.sub-menu').css('display','none');
+			}else if($('.sub-menu').css('display') == 'none'){
+				$('.sub-menu').css('display','block');
+			}
+		})
 	});
 });
